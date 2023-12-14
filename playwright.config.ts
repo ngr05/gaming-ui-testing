b885dbc83bd8f2bfc2ff72a547558842f6b33b80
Nick Green <Nicholas.Green@skybettingandgaming.com>
2023-12-14 09:47:53 +0000

    scaled out the test workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    workers: 10,
+    workers: 20,
     // workers: process.env.CI ? 2 : 10,
     // workers: 5,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
